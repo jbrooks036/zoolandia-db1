94f2156fa99c123eebedb2c5e224f37dec8141d6
Jo Ann Brooks <joannbrooks036@gmail.com>
2016-03-29 12:47:45 -0500

    initial code for Entity Framework and migrations project

---
+++ src/zoolandia-db1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+
+namespace zoolandia_db1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            // seed the database
+        }
+
+        public class Animal
+        {
+            public Animal(string animalType)
+            {
+
+            }
+            public int AnimalId { get; set; }
+            public string AnimalName { get; set; }
+            public float AnimalWeight { get; set; }
+        }
+
+        public class AnimalContext : DbContext
+        {
+            // IDbSet, IQueryable
+            public virtual DbSet<Animal> Animals { get; set; }
+        }
+
+    }
+}
